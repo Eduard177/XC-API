131ece845cd43facf2fda3d3cb0eac60e216e735
Jean carlos Perez <thejc2485@gmail.com>
2020-02-29 13:47:25 -0400

    feat: Auth  module implementation. - Login endpoint added. - Register endpoint added. - AdminGuard added. - Jwt Strategy added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from './modules/config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, ConfigModule, DatabaseModule],
+  imports: [UserModule, ConfigModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
