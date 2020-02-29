2796b8c43750ae8e1b8a9aa2c345651cbea796b7
Jean carlos Perez <thejc2485@gmail.com>
2020-02-29 17:01:55 -0400

    fix: User and Auth module. - service tests added. - controller tests added.

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserEntity } from '../user/entities/user.entity';
+import { RemoveOptions, SaveOptions } from 'typeorm';
+import { JwtStrategy } from './strategies/jwt.stategy';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '../config/config.service';
+import { UserService } from '../user/user.service';
+import { IUserRegister } from './interfaces/user-register.interface';
+import { IUserLogin } from './interfaces/user-login.interface';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
-
+  let service: AuthService;
+  const user: UserEntity = {
+    cellphone: '',
+    companyCode: '',
+    email: '',
+    fullName: '',
+    id: 0,
+    imageUrl: '',
+    office: '',
+    password: '',
+    position: '',
+    role: '',
+    hasId(): boolean {
+      return false;
+    },
+    reload(): Promise<void> {
+      return undefined;
+    },
+    remove(options?: RemoveOptions): Promise<any> {
+      return undefined;
+    },
+    save(options?: SaveOptions): Promise<any> {
+      return undefined;
+    },
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtStrategy,
+        {
+          provide: JwtService,
+          useFactory: () => ({
+            sign: jest.fn(() => 'random-string'),
+          }),
+        },
+        {
+          provide: ConfigService,
+          useFactory: () => ({
+            get: jest.fn(() => 'random-string'),
+          }),
+        },
+        {
+          provide: UserService,
+          useFactory: () => ({
+            findOneByEmail: jest.fn(() => user ),
+            create: jest.fn(() => user ),
+          }),
+        },
+      ],
     }).compile();
-
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('register', () => {
+    const payload: IUserRegister = {
+      cellphone: '',
+      companyCode: '',
+      email: '',
+      fullName: '',
+      imageUrl: '',
+      office: '',
+      password: 'string',
+      position: '',
+      role: '',
+    };
+    expect(controller.userRegister(payload)).not.toBeNull();
+  });
+
+  it('login', () => {
+    const payload: IUserLogin = { email: '', password: 'random-string' };
+    expect(controller.userLogin(payload)).not.toBeNull();
   });
 });
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.stategy';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '../config/config.service';
+import { UserService } from '../user/user.service';
+import { UserEntity } from '../user/entities/user.entity';
+import { RemoveOptions, SaveOptions } from 'typeorm';
+import { IJwtPayload } from './interfaces/jwt-payload.interface';
+import { IUserRegister } from './interfaces/user-register.interface';
+import { IUserLogin } from './interfaces/user-login.interface';
 
 describe('AuthService', () => {
   let service: AuthService;
-
+  const user: UserEntity = {
+    cellphone: '',
+    companyCode: '',
+    email: '',
+    fullName: '',
+    id: 0,
+    imageUrl: '',
+    office: '',
+    password: '',
+    position: '',
+    role: '',
+    hasId(): boolean {
+      return false;
+    },
+    reload(): Promise<void> {
+      return undefined;
+    },
+    remove(options?: RemoveOptions): Promise<any> {
+      return undefined;
+    },
+    save(options?: SaveOptions): Promise<any> {
+      return undefined;
+    },
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtStrategy,
+        {
+          provide: JwtService,
+          useFactory: () => ({
+            sign: jest.fn(() => 'random-string'),
+          }),
+        },
+        {
+          provide: ConfigService,
+          useFactory: () => ({
+            get: jest.fn(() => 'random-string'),
+          }),
+        },
+        {
+          provide: UserService,
+          useFactory: () => ({
+            findOneByEmail: jest.fn(() => user ),
+            create: jest.fn(() => user ),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +68,39 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('validateByJwt', () => {
+    const payload: IJwtPayload = { email: '', type: '' };
+    expect(service.validateByJwt(payload)).not.toBeNull();
+  });
+
+  it('createJwtPayload', () => {
+    const payload: IJwtPayload = { email: '', type: '' };
+    expect(service.createJwtPayload(payload)).not.toBeNull();
+  });
+
+  it('userRegister', () => {
+      const payload: IUserRegister = {
+        cellphone: '',
+        companyCode: '',
+        email: '',
+        fullName: '',
+        imageUrl: '',
+        office: '',
+        password: 'string',
+        position: '',
+        role: '',
+      };
+      expect(service.userRegister(payload)).not.toBeNull();
+  });
+
+  it('hashPassword', () => {
+    expect(service.hashPassword('string')).not.toBeNull();
+  });
+
+  it('validateUserByPassword', () => {
+    const payload: IUserLogin = { email: '', password: 'random-string' };
+    expect(service.validateUserByPassword(payload)).not.toBeNull();
   });
 });
