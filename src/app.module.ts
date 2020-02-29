5e67561abfe9c26f804cfa8007646d54cb18c191
alamalcantara <alamalcantara99@gmail.com>
2020-02-29 14:17:25 -0400

    feat: generating reports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from './modules/config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
-  imports: [UserModule, ConfigModule, DatabaseModule],
+  imports: [UserModule, ConfigModule, DatabaseModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
