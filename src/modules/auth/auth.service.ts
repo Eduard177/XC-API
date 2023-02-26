cf939752c437bee0f2a0d6247f2ebe0a649e7405
Eduard177 <eduarro2001@gmail.com>
2023-02-26 01:29:48 -0400

    fix encrypt password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,21 @@
-import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserEntity } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
 import { IJwtPayload } from './interfaces/jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
 import { Configuration } from '../config/config.keys';
 import { ConfigService } from '../config/config.service';
-import * as bcrypt from 'bcrypt';
+import { compareSync, hash } from 'bcrypt';
 import { IUserRegister } from './interfaces/user-register.interface';
 import { IUserLogin } from './interfaces/user-login.interface';
 
 @Injectable()
 export class AuthService {
-
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
@@ -39,57 +43,32 @@ export class AuthService {
         throw new ForbiddenException('USER_EXIST');
       }
 
-      const password = await this.hashPassword(payload.password);
-      return await this.userService.create(
-        {
-          email: payload.email,
-          role: payload.role,
-          fullName: payload.fullName,
-          office: payload.office,
-          companyCode: payload.companyCode,
-          imageUrl: payload.imageUrl,
-          position: payload.position,
-          cellphone: payload.cellphone,
-          password,
-        },
-      );
+      const password = await hash(payload.password, 10);
+      return await this.userService.create({
+        ...payload,
+        password,
+      });
     } catch (e) {
       throw e;
     }
   }
 
-  async hashPassword(password: string): Promise<object> {
-    return new Promise(((resolve, reject) => {
-      bcrypt.genSalt(10, async (err, salt) => {
-        // tslint:disable-next-line:no-shadowed-variable
-        bcrypt.hash(password, salt, (err, hash) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(hash);
-        });
-      });
-    }));
-  }
-
   async validateUserByPassword(payload: IUserLogin): Promise<object> {
-    const user: UserEntity = await this.userService.findOneByEmail(payload.email);
+    const user: UserEntity = await this.userService.findOneByEmail(
+      payload.email,
+    );
 
     if (!user) {
       throw new NotFoundException();
     }
 
-    const isPasswordValid = new Promise(((resolve, reject) => {
-      bcrypt.compare(payload.password, user.password, (err: any, isMatch: any) => {
-        if (err) { reject(err); }
-        resolve(isMatch);
-      });
-    }));
+    const isPasswordValid = compareSync(payload.password, user.password);
 
     if (isPasswordValid) {
       return {
-        jwt: await this.createJwtPayload({
-        email: user.email}),
+        jwt: this.createJwtPayload({
+          email: user.email,
+        }),
         user,
       };
     } else {
